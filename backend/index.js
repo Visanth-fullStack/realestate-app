65a9df5d45b14cb104c7a5287efcf58113fbb02a
unknown <visanth.kallazhi@lyveglobal.com>
2024-07-21 22:19:15 +0400

    validations_enable

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
-.then(() => console.log('Connected to MongoDB'))
+.then(() => console.log('Connected to MongoDB successKs'))
 .catch(err => console.error('MongoDB connection error:', err));
 
 
